52f39532264ae43a318f6263c3f94cf8ca67317c
Vladislav Ovchinnikov <ovchinnikovva@okta-solutions.com>
2020-03-17 21:55:27 +0300

    Удален баг репозитория

--- src/main/java/ml/socshared/adapter/fb/repository/TestRepository.java
+++
@@ -1,8 +0,0 @@
-package ml.socshared.adapter.fb.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TestRepository extends JpaRepository<TestObject, Integer> {
-}
