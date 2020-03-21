395f9411dfcd64c22156c38b03a37f6672a5d6b7
Vladislav Ovchinnikov <ovchinnikovva@okta-solutions.com>
2020-03-21 23:23:56 +0300

    Пофиксил тест

--- src/test/java/ml/socshared/adapter/fb/ApplicationContextTest.java
+++ src/test/java/ml/socshared/adapter/fb/ApplicationContextTest.java
@@ -5,7 +5,11 @@ package ml.socshared.adapter.fb;
 
 import ml.socshared.adapter.fb.integration.AbstractIT;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
 import static org.junit.Assert.*;
 
 public class ApplicationContextTest extends AbstractIT {
--- src/test/java/ml/socshared/adapter/fb/integration/AbstractIT.java
+++ src/test/java/ml/socshared/adapter/fb/integration/AbstractIT.java
@@ -2,15 +2,18 @@ package ml.socshared.adapter.fb.integration;
 
 import lombok.Getter;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 import static ml.socshared.adapter.fb.config.Constants.TEST_PROFILE;
 
 @ExtendWith(SpringExtension.class)
+@RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 @ActiveProfiles(TEST_PROFILE)
 public abstract class AbstractIT {
