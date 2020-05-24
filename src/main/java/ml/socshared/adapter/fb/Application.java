bd7235c0e6e9660258aca83115359e31c24029c8
Владислав Овчинников <vladovchinnikov950@gmail.com>
2020-05-24 05:45:51 +0300

    Поправил ошибки и баги

--- src/main/java/ml/socshared/adapter/fb/Application.java
+++ src/main/java/ml/socshared/adapter/fb/Application.java
@@ -6,9 +6,11 @@ package ml.socshared.adapter.fb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.social.config.annotation.EnableSocial;
 
+@EnableFeignClients
 @SpringBootApplication
 public class Application {
 
