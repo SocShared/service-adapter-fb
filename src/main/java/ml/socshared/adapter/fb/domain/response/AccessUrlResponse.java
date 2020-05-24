6000e4b901456f35d0032920c636bd66a5530bd7
Владислав Овчинников <vladovchinnikov950@gmail.com>
2020-05-24 06:01:03 +0300

    Пофиксил JWT

--- src/main/java/ml/socshared/adapter/fb/domain/response/AccessUrlResponse.java
+++ src/main/java/ml/socshared/adapter/fb/domain/response/AccessUrlResponse.java
@@ -1,7 +1,11 @@
 package ml.socshared.adapter.fb.domain.response;
 
 import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Builder
 public class AccessUrlResponse {
 
